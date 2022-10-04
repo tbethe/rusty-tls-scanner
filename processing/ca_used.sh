a87b7d36d6e5df1dbf05dcdaa3b04881a8d6d85a
Timme Bethe <git@timmebethe.nl>
2022-10-04 15:10:37 +0200

    Print total amount of certs

--- processing/ca_used.sh
+++ processing/ca_used.sh
@@ -17,5 +17,6 @@ certs=`cat "$1" | jq '[.[] | select(.tls_connection_info.valid_chain=="ok")]'`
 
 # 2 + 3 + 4
 total=$(echo $certs | jq 'length')
+echo Total: $total
 echo "$certs" | jq -c '[.[] | { "domain" : .domain, "chain" : .tls_connection_info.cert_chain.chain[-1].issuer_name }] | unique | .[] | .chain' | sort -n | uniq -c | sort -rn | head -n 10 | sed -E 's/[ \t]*([0-9]+)[ ]/\1=/' | awk -v tot=$total 'BEGIN {FS="="; OFS="\t"; printf("%100s | %5s | %s\n", "CA", "Count", "%X")} {printf("%100s | %5s | %.3f\n", $2, $1, ($1/tot))}'
 
